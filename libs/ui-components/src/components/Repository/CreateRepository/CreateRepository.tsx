725d07d60db49d45e24adc48dcd187d22e0cd28d
Liat Berkovich <lberkovi@redhat.com>
2026-05-24 21:47:19 +0300

    Update CreateRepository.tsx

--- libs/ui-components/src/components/Repository/CreateRepository/CreateRepository.tsx
+++ libs/ui-components/src/components/Repository/CreateRepository/CreateRepository.tsx
@@ -13,6 +13,7 @@ import {
   Title,
 } from '@patternfly/react-core';
 
+
 import { Repository, ResourceSync, ResourceSyncList } from '@flightctl/types';
 
 import { useTranslation } from '../../../hooks/useTranslation';
